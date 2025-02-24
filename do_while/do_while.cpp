62a1413c2957d92430d30fccc885a4a16487f03c
Александр Важнёв <vazhnevalex@gmail.com>
2025-02-24 17:44:47 +0300

    Добавьте файлы проекта.

---
+++ do_while/do_while.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int number, id = 1;
+	cout << "Введите number = ";
+	cin >> number;
+	do // выводим на экран id а затем увеличивает
+		cout << id++ << "\t"; //её значение на еденицу
+		while (id <= number); //до тех пор пока меньше id <= number 
+}
